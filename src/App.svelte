fab7edb0fe8c3836a36f613f1959b3d035c573a4
Wolf Developer <lupo.andrea.93@gmail.com>
2021-02-25 08:35:36 +0100

    Simple slots

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
 	import Product from "./Product.svelte";
+	import Modal from "./Modal.svelte";
 
 	let products = [
         {
@@ -25,3 +26,10 @@
 		on:add-to-cart="{addToCard}"
 		on:delete="{deleteProduct}"/>
 {/each}
+
+<Modal>
+	<h1>
+		Hello!
+	</h1>	
+	<p>This works!</p>
+</Modal>
